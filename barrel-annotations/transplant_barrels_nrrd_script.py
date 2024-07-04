a0a54f40defcda029e52f6823ec977f6dc2dc7f9
abisi <axel.bisi@epfl.ch>
2024-07-04 17:30:58 +0200

    update args

--- barrel-annotations/transplant_barrels_nrrd_script.py
+++ barrel-annotations/transplant_barrels_nrrd_script.py
@@ -2,10 +2,18 @@ import argparse
 import numpy as np
 from voxcell import VoxelData
 
-def transplant_annotations(data_path, resolution):
+def transplant_annotations(data_path, annotation_path, resolution):
+    """
+    Transplant pregenerated barrel annotations into an annotation file.
+    :param data_path:
+    :param annotation_path:
+    :param resolution:
+    :return:
+    """
+    print('Transplanting barrel annotations into annotation file...')
     # Define paths based on provided data_path and resolution
     annotation_barrels_path = f"{data_path}/annotation_barrels.nrrd"
-    annotation_path = f"{data_path}/annotation_{resolution}.nrrd"
+    annotation_path = f"{annotation_path}/annotation_{resolution}.nrrd"
     output_path = f"{data_path}/annotation_barrels_{resolution}.nrrd"
 
     # Load the necessary nrrd files
@@ -35,12 +43,14 @@ def transplant_annotations(data_path, resolution):
     )
 
     # Save the updated annotation data to the output path
+    print('Saving updated annotation data to output path...')
     barrel_nrrd.save_nrrd(output_path)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Transplant pregenerated barrel annotations into an annotation file.')
-    parser.add_argument('data_path', type=str, help='Base path to the data files')
+    parser.add_argument('data_path', type=str, help='Path to new data files')
+    parser.add_argument('annotation_path', type=str, help='Path to the original Allen annotation file')
     parser.add_argument('resolution', type=str, help='Resolution identifier to determine which annotation file to use')
 
     args = parser.parse_args()
-    transplant_annotations(args.data_path, args.resolution)
+    transplant_annotations(args.data_path, args.annotation_path, args.resolution)
